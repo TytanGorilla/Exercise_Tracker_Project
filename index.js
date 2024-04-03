95ee074a512da52607afab273049d8e878295a9b
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2024-04-03 12:21:46 +0200

    feat:Successfully connected to MongoDB via Atlas https://cloud.mongodb.com/

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
